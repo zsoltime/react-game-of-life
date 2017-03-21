5751be474f04d4cf1a4ca28c94cfc6f4ba0e2d75
Zsolt Meszaros <zsolt@sowad.com>
2017-03-21 14:15:04 +0100

    Initial commit

---
+++ app/components/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const About = () => (
+  <div>
+    <h1>About</h1>
+    <p>This is my take on Free Code Camp&raquo;s Game of Life built using React.js.</p>
+  </div>
+);
+
+export default About;
